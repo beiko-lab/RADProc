dd765a325dd3c13175f0b86846b40cff2ab1c042
Praveen <pravindran@dal.ca>
2019-04-03 11:59:29 -0300

    Update RADProc.cc
    fixed bug

--- Install/src/RADProc.cc
+++ Install/src/RADProc.cc
@@ -96,7 +96,7 @@ int parse_command_line(int argc, char* argv[]) {
 	// getopt_long stores the option index here.
 	int option_index = 0;
         count++;
-	c = getopt_long(argc, argv, "haf:t:o:p:x:M:m:S:D:", long_options, &option_index);
+	c = getopt_long(argc, argv, "haf:t:o:p:x:M:m:n:S:D:", long_options, &option_index);
 	// Detect the end of the options.
 	
         if (c == -1)
